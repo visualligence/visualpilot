a3909bf3d90da2d6d26a8eeac6b088bcd70e16c6
Rober <rober@visualligence.com>
2016-03-11 18:14:56 +0100

    fast mode tried

--- adapters/displays/ssd1306_128x64.h
+++ adapters/displays/ssd1306_128x64.h
@@ -5,11 +5,11 @@ class Display{
 	U8GLIB_SSD1306_128X64 u8g;
 
 public:
-	// 	u8g(U8G_I2C_OPT_DEV_0|U8G_I2C_OPT_NO_ACK|U8G_I2C_OPT_FAST);	// Fast I2C / TWI
-	// 	u8g(U8G_I2C_OPT_NO_ACK);	// Display which does not send AC
+	// u8g( U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0 ) // I2C / TWI
+	// u8g( U8G_I2C_OPT_NO_ACK ) // Display which does not send AC
 
 	Display()
-	: u8g( U8G_I2C_OPT_NONE | U8G_I2C_OPT_DEV_0 ) // I2C / TWI
+	: u8g( U8G_I2C_OPT_DEV_0 | U8G_I2C_OPT_NO_ACK | U8G_I2C_OPT_FAST ) // Fast I2C / TWI
 	{
 	}
 
