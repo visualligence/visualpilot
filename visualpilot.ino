373a0e2c84afc15e21d425eddca82a53321d1fda
Rober <rober@visualligence.com>
2016-03-18 15:20:41 +0100

    imu not running at 400khz. 300khz is a better option

--- visualpilot.ino
+++ visualpilot.ino
@@ -1,44 +1,60 @@
-#include "./adapters/displays/ssd1306_128x64.h"
+//#include "./adapters/i2c.h"
+
+#include "./adapters/displays/ssd1306_128x64_esp.h"
 #include "./adapters/pwm/i2c_12bit.h"
 #include "./adapters/imu/nunchuk.h"
 
-uint8_t loop_number = 0;
+uint32_t loop_number = 0;
 
 uint16_t loop_start = 0;
 uint16_t loop_end = 1;
+uint16_t loop_duration = 1;
 
 Display display = Display();
-PWM pwm = PWM();
 IMU imu = IMU();
+PWM pwm = PWM();
 
 void setup(void) {
-	Wire.setClock( 400000L );
+	Wire.begin();
+	Wire.setClock( 300000L );
 
 	display.setup();
-	pwm.setup();
 	imu.setup();
+	pwm.setup();
 }
 
 void draw(void) {
-	display.h_line( 0,  0, loop_number % 128 );
+	display.h_line( 0, 63, loop_number % 128 );
 
-	int line_height = 8;
+	display.text( 128,  0, loop_number, Display::RIGHT );
+	display.text( 128, 10, 1000000L / loop_duration, Display::RIGHT );
+	display.text( 128, 20, loop_duration, Display::RIGHT );
 
-	display.h_line( 0, 1 * line_height, map( imu.a.x,  0, 1024, 0, 127 ) );
-	display.h_line( 0, 2 * line_height, map( imu.a.y,  0, 1024, 0, 127 ) );
-	display.h_line( 0, 3 * line_height, map( imu.a.z,  0, 1024, 0, 127 ) );
+	atan2( imu.a.y, imu.a.x );
 
-	display.text(  0, 35, "mfps" );
-	display.text( 25, 35, 1000000L / ( loop_end - loop_start ) );
+	int radar_size = 30;
+
+	v3<uint16_t> radar;
+
+	radar.x = map( imu.a.x,  0, 1024, 0, radar_size );
+	radar.y = map( imu.a.y,  0, 1024, 0, radar_size );
+	radar.z = map( imu.a.z,  0, 1024, 0, radar_size );
+
+	display.line( radar_size/2, radar_size/2, radar.x, radar.y );
+	display.line( radar_size/2, radar_size,   radar.x, radar_size/2 + radar.z );
 }
 
 void loop(void) {
 	imu.update();
+	pwm.set_angle( 0, loop_number % 181 );
 	display.loop( draw );
 
 	loop_number ++;
 	loop_start = loop_end;
 	loop_end = millis();
+	loop_duration = loop_end - loop_start;
 }
+
+
 
 
